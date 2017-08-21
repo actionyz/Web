0efdcc11111162166ae14c2ebeb8158a308824c9
z y <z y>
2017-08-21 10:24:20 +0800

    add

---
+++ little tricks/trick 1.php
@@ -0,0 +1,11 @@
+<?php  
+error_reporting(0);
+$flag = "flag{unserialize_by_virink}";
+highlight_file(__file__);
+if(isset($_GET['args'])){
+    $args = $_GET['args'];
+    if(!preg_match("/^\w+$/",$args)){
+        die("args error!");
+    }
+    eval("var_dump($$args);");
+}
